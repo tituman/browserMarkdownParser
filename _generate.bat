e8549cb3b3f0f5dc297f5669b64e307e76f983d9
tituman <tituman@users.noreply.github.com>
2015-02-10 09:37:06 +0100

    gen

--- _generate.bat
+++
@@ -1,2 +0,0 @@
-FOR %%X IN (*.md) DO ( TYPE _mediaDocu\_offpre.html %%X _mediaDocu\_offpost.html > "%%X.html" && start chrome "%~dp0%%X.html" )
-FOR %%X IN (*.pnd) DO ( TYPE _mediaDocu\_offpre.html %%X _mediaDocu\_offpost.html > "%%X.html" && start chrome "%~dp0%%X.html" )
