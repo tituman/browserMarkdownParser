026105967402b9e0fd543e9182cc8ae77bd25cb9
tituman <krobeczu@gmail.com>
2017-07-24 10:56:46 +0200

    generate feilen

--- dist/_generate.bat
+++ dist/_generate.bat
@@ -1,2 +1,3 @@
-FOR %%X IN (*.md) DO ( TYPE _mediaDocu\_offpre.html %%X _mediaDocu\_offpost.html > "%%X.html" && start chrome "%~dp0%%X.html" )
-FOR %%X IN (*.pnd) DO ( TYPE _mediaDocu\_offpre.html %%X _mediaDocu\_offpost.html > "%%X.html" && start chrome "%~dp0%%X.html" )
+FOR %%X IN (*.md) DO ( TYPE _mediaDocu\_offpre.html "%%X" _mediaDocu\_offpost.html > "%%~nX.html" && start chrome "%~dp0%%~nX.html" )
+
+FOR %%X IN (*.pnd) DO ( TYPE _mediaDocu\_offpre.html "%%X" _mediaDocu\_offpost.html > "%%~nX.html" && start chrome "%~dp0%%~nX.html" )
